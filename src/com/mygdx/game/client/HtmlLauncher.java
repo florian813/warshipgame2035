4c797392dcfe8659bad2a186145cc94a77be3778
florian813 <56045484+florian813@users.noreply.github.com>
2022-04-05 22:58:54 +0200

    Add files via upload

---
+++ src/com/mygdx/game/client/HtmlLauncher.java
@@ -0,0 +1,22 @@
+package com.mygdx.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.mygdx.game.warshipsGames2035;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                return new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                //return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new warshipsGames2035();
+        }
+}
